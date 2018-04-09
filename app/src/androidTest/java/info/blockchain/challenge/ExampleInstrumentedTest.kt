892b6c817010d44731377610462086b2670652b7
Alan Evans <thealanevans@gmail.com>
2018-04-09 20:13:18 -0300

    Rename package (#2)

--- app/src/androidTest/java/info/blockchain/challenge/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/info/blockchain/challenge/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package challenge.blockchain.info.blockchainchallenge
+package info.blockchain.challenge
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("challenge.blockchain.info.blockchainchallenge", appContext.packageName)
+        assertEquals("info.blockchain.challenge", appContext.packageName)
     }
 }
