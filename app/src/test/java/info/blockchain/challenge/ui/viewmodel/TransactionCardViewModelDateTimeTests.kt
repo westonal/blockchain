bf1d9a7fc95c74a00bf8a37d7ca9ad32afdb3dc0
Alan Evans <thealanevans@gmail.com>
2018-04-11 08:26:44 -0300

    Transaction View Model Date formatting & tests

---
+++ app/src/test/java/info/blockchain/challenge/ui/viewmodel/TransactionCardViewModelDateTimeTests.kt
@@ -0,0 +1,33 @@
+package info.blockchain.challenge.ui.viewmodel
+
+import org.amshove.kluent.`should equal`
+import org.junit.Test
+import java.util.*
+
+class TransactionCardViewModelDateTimeTests {
+
+    @Test
+    fun `date time formatted to the locale US`() {
+        Locale.setDefault(Locale.US)
+        givenTransactionOfDateTime(
+                date(2018, 4, 11, 12, 23, 45))
+                .dateTime `should equal` "Apr 11, 2018 12:23:45 PM"
+    }
+
+    @Test
+    fun `date time formatted to the locale UK`() {
+        Locale.setDefault(Locale.UK)
+        givenTransactionOfDateTime(
+                date(2017, 12, 31, 8, 2, 1))
+                .dateTime `should equal` "31-Dec-2017 08:02:01"
+    }
+
+    private fun date(year: Int, month: Int, day: Int, hour: Int = 0, minute: Int = 0, second: Int = 0) =
+            Calendar.getInstance().apply {
+                set(year, month - 1, day, hour, minute, second)
+            }.time
+
+    private fun givenTransactionOfDateTime(date: Date) = TransactionCardViewModel(0L.satoshiToBtc(),
+            date = date)
+
+}
