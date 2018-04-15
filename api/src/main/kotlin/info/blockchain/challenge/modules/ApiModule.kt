ac8cb0ed9d48bedf51c88444951ce8f57e11e67d
Alan Evans <thealanevans@gmail.com>
2018-04-15 10:20:03 -0300

    Rename service

--- api/src/main/kotlin/info/blockchain/challenge/modules/ApiModule.kt
+++ api/src/main/kotlin/info/blockchain/challenge/modules/ApiModule.kt
@@ -1,6 +1,6 @@
 package info.blockchain.challenge.modules
 
-import info.blockchain.challenge.api.MultiAddress
+import info.blockchain.challenge.api.BlockchainService
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
@@ -8,5 +8,5 @@ import org.kodein.di.generic.provider
 import retrofit2.Retrofit
 
 val apiModule = Kodein.Module {
-    bind<MultiAddress>() with provider { instance<Retrofit>().create(MultiAddress::class.java) }
+    bind<BlockchainService>() with provider { instance<Retrofit>().create(BlockchainService::class.java) }
 }
--- app/src/test/java/info/blockchain/challenge/ui/WalletMviDialogTests.kt
+++ app/src/test/java/info/blockchain/challenge/ui/WalletMviDialogTests.kt
@@ -6,7 +6,7 @@ import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.verifyNoMoreInteractions
 import com.nhaarman.mockito_kotlin.verifyZeroInteractions
 import info.blockchain.challenge.R
-import info.blockchain.challenge.api.MultiAddress
+import info.blockchain.challenge.api.BlockchainService
 import info.blockchain.challenge.api.Result
 import info.blockchain.challenge.api.Transaction
 import info.blockchain.challenge.api.Wallet
@@ -28,7 +28,7 @@ class WalletMviDialogTests {
 
     @Test
     fun `given no xpubs, no view models are created and service is not called`() {
-        val service: MultiAddress = mock()
+        val service = mock<BlockchainService>()
         WalletMviDialog(service, intents)
                 .viewModel.test().assertNoValues()
         verifyZeroInteractions(service)
@@ -37,7 +37,7 @@ class WalletMviDialogTests {
     @Test
     fun `given a single xpub, the service is called and result is mapped to a view model`() {
         val xpub = "xpub12345"
-        val service: MultiAddress = givenServiceReturns(xpub, Transaction(result = 2L))
+        val service = givenServiceReturns(xpub, Transaction(result = 2L))
         val walletModule = WalletMviDialog(service, intents)
         val testObserver = walletModule
                 .viewModel.test()
@@ -50,25 +50,25 @@ class WalletMviDialogTests {
     @Test
     fun `given a single xpub, the service is called just once`() {
         val xpub = "xpub12345"
-        val service: MultiAddress = givenServiceReturns(xpub)
+        val service = givenServiceReturns(xpub)
         val walletModule = WalletMviDialog(service, intents)
         walletModule.viewModel.test()
         intents.onNext(NewXpubIntent(xpub))
-        verify(service).multiaddr(xpub)
+        verify(service).multiAddress(xpub)
         verifyNoMoreInteractions(service)
     }
 
     @Test
     fun `given the same xpub twice, the service is called just once`() {
         val xpub = "xpub12345"
-        val service: MultiAddress = givenServiceReturns(xpub)
+        val service = givenServiceReturns(xpub)
         val walletModule = WalletMviDialog(service, intents)
 
         walletModule.viewModel.test()
         intents.onNext(NewXpubIntent(xpub))
         intents.onNext(NewXpubIntent(xpub))
 
-        verify(service).multiaddr(xpub)
+        verify(service).multiAddress(xpub)
         verifyNoMoreInteractions(service)
     }
 
@@ -76,9 +76,9 @@ class WalletMviDialogTests {
     fun `given two xpubs, the service is called twice`() {
         val xpub1 = "xpub12345"
         val xpub2 = "xpub23456"
-        val service: MultiAddress = mock {
-            on { it.multiaddr(xpub1) } `it returns` apiResult()
-            on { it.multiaddr(xpub2) } `it returns` apiResult()
+        val service = mock<BlockchainService> {
+            on { it.multiAddress(xpub1) } `it returns` apiResult()
+            on { it.multiAddress(xpub2) } `it returns` apiResult()
         }
         val walletModule = WalletMviDialog(service, intents)
 
@@ -86,15 +86,15 @@ class WalletMviDialogTests {
         intents.onNext(NewXpubIntent(xpub1))
         intents.onNext(NewXpubIntent(xpub2))
 
-        verify(service).multiaddr(xpub1)
-        verify(service).multiaddr(xpub2)
+        verify(service).multiAddress(xpub1)
+        verify(service).multiAddress(xpub2)
         verifyNoMoreInteractions(service)
     }
 
     @Test
     fun `given a single xpub, the service is called zero times if not subscribed to`() {
         val xpub = "xpub12345"
-        val service: MultiAddress = givenServiceReturns(xpub)
+        val service = givenServiceReturns(xpub)
         WalletMviDialog(service, intents)
         intents.onNext(NewXpubIntent(xpub))
         verifyZeroInteractions(service)
@@ -103,7 +103,7 @@ class WalletMviDialogTests {
     @Test
     fun `given a single xpub, the service is called and result with 2 transactions is mapped to a view model`() {
         val xpub = "xpub23456"
-        val service: MultiAddress = givenServiceReturns(xpub, Transaction(result = 2L), Transaction(result = 3L))
+        val service = givenServiceReturns(xpub, Transaction(result = 2L), Transaction(result = 3L))
         val walletModule = WalletMviDialog(service, intents)
         val testObserver = walletModule
                 .viewModel.test()
@@ -118,7 +118,7 @@ class WalletMviDialogTests {
     @Test
     fun `if the service throws an exception, a single ErrorCard is shown`() {
         val xpub = "xpub23456"
-        val service: MultiAddress = givenServiceThrows()
+        val service = givenServiceThrows()
         val walletModule = WalletMviDialog(service, intents)
         val testObserver = walletModule
                 .viewModel.test()
@@ -137,8 +137,8 @@ class WalletMviDialogTests {
     @Test
     fun `when there is an error, can retry`() {
         val xpub = "xpub23456"
-        val service: MultiAddress = mock {
-            on { multiaddr(any()) }
+        val service = mock<BlockchainService> {
+            on { multiAddress(any()) }
                     .`it returns`(Single.error { RuntimeException() })
                     // Note: set up to fail once, then succeed
                     .`it returns`(apiResult(Transaction(result = 2L)))
@@ -163,14 +163,14 @@ class WalletMviDialogTests {
         }
 
         testObserver.valueCount() `should be` 2
-        verify(service, times(2)).multiaddr(xpub)
+        verify(service, times(2)).multiAddress(xpub)
     }
 
     @Test
     fun `can be refreshed`() {
         val xpub = "xpub34567"
-        val service: MultiAddress = mock {
-            on { multiaddr(any()) }
+        val service = mock<BlockchainService> {
+            on { multiAddress(any()) }
                     .`it returns`(apiResult(Transaction(result = 2L)))
                     // Note: set up to return with different results
                     .`it returns`(apiResult(Transaction(result = 1L), Transaction(result = 2L)))
@@ -199,17 +199,15 @@ class WalletMviDialogTests {
         }
 
         testObserver.valueCount() `should be` 2
-        verify(service, times(2)).multiaddr(xpub)
+        verify(service, times(2)).multiAddress(xpub)
     }
 
-    private fun givenServiceThrows(): MultiAddress = mock {
-        on { multiaddr(any()) } `it returns` Single.error<Result> { RuntimeException() }
+    private fun givenServiceThrows() = mock<BlockchainService> {
+        on { multiAddress(any()) } `it returns` Single.error<Result> { RuntimeException() }
     }
 
-    private fun givenServiceReturns(xpub: String, vararg transactions: Transaction): MultiAddress {
-        return mock {
-            on { it.multiaddr(xpub) } `it returns` apiResult(*transactions)
-        }
+    private fun givenServiceReturns(xpub: String, vararg transactions: Transaction) = mock<BlockchainService> {
+        on { it.multiAddress(xpub) } `it returns` apiResult(*transactions)
     }
 
     private fun apiResult(vararg transactions: Transaction) =
